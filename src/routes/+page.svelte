9772b0be24a5a3b150aa8aecdbd862a2ba46a48f
karnikaavelumani <60043611+karnikaavelumani@users.noreply.github.com>
2023-10-17 15:46:55 -0700

    Set up pages and favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Spacing from '$lib/spacing.svelte';
+  import Hero from './hero.svelte';
+  import Bio from './bio.svelte';
+  import Leadership from './leadership.svelte';
+  import Experience from './experience.svelte';
+  import Skills from './skills.svelte';
+  import Languages from './languages.svelte';
+  import Contact from './contact.svelte';
+</script>
+
+<svelte:head>
+  <title>Karni</title>
+</svelte:head>
+
+<Spacing --min="175px" --med="200px" --max="200px" />
+
+<Hero />
+
+<Spacing --min="120px" --med="175px" --max="200px" />
+
+<Bio />
+
+<Spacing --min="120px" --med="175px" --max="200px" />
+
+<Leadership />
+
+<Spacing --min="120px" --med="175px" --max="200px" />
+
+<Experience />
+
+<Spacing --min="120px" --med="175px" --max="200px" />
+
+<Skills />
+
+<Spacing --min="120px" --med="175px" --max="200px" />
+
+<Languages />
+
+<Spacing --min="120px" --med="175px" --max="200px" />
+
+<Contact />
+
+<Spacing --min="75px" --med="100px" --max="120px" />
